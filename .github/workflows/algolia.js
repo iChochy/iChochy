06c7b7d3036c93bc36a6cc446e280ec80d99996f
iChochy <ichochy@qq.com>
2021-06-12 13:38:02 +0800

    Update algolia.js

--- .github/workflows/algolia.js
+++ .github/workflows/algolia.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const APPLICATION_ID = process.env.secrets.APPLICATION_ID;
 const ADMIN_API_KEY = process.env.secrets.ADMIN_API_KEY;
 const INDEX_NAME = process.env.secrets.INDEX_NAME;
-const DATA_URL = process.env.secrets.DATA_URL;
+const FILE_PATH = process.env.secrets.FILE_PATH;
 
 const client = algoliasearch(APPLICATION_ID, ADMIN_API_KEY)
 const index = client.initIndex(INDEX_NAME)
 
 try {
-  const data = fs.readFileSync(DATA_URL, 'utf8')
+  const data = fs.readFileSync(FILE_PATH, 'utf8')
   const objects = JSON.parse(data);
   index
     .saveObjects(objects, { autoGenerateObjectIDIfNotExist: true })
