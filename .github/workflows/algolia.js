824ae425de23d9698b2a26edecaba75f29b3b68d
iChochy <ichochy@qq.com>
2021-06-12 23:57:28 +0800

    Update algolia.js

--- .github/workflows/algolia.js
+++ .github/workflows/algolia.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const APPLICATION_ID = process.env.APPLICATION_ID;
 const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
 const INDEX_NAME = process.env.INDEX_NAME;
-const FILE_PATH = process.env.RUNNER_WORKSPACE;
+const FILE_PATH = process.env.FILE_PATH;
 
 const client = algoliasearch(APPLICATION_ID, ADMIN_API_KEY)
 const index = client.initIndex(INDEX_NAME)
